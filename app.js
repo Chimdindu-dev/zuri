12e67a13c8d144602e812399918a1824aeedc768
Chimdindu-dev <cchimdindu@gmail.com>
2022-11-05 10:04:43 +0200

    last update

--- app.js
+++ app.js
@@ -10,65 +10,85 @@ app.post("/", (req, res) => {
   console.log(req.body);
   let { operation_type, x, y } = req.body;
 
-  const tired = JSON.stringify(req.body.operation_type);
-  const myArray = tired.split(" ");
+  //const tired = JSON.stringify(req.body.operation_type);
+  const myArray = req.body.operation_type.split(" ");
 
   const addition = ["add","top up","positive","join","plus","combine","+","sum","addition",];
   const subtraction = ["minus","negative","smaller","less","remove","subtract","-","deficient","subtraction",];
   const multiplication = ["times", "*", "multiply", "multiplication"];
 
-  let lenadd = addition.length;
-  let lensub = subtraction.length;
-  let lenmul = multiplication.length;
-  let lenarr = myArray.length;
-
-  for(var j=0;j<lenarr;j++){
-    for(var a=0;a<lenadd;a++){
-      if(myArray[j]==addition[a]){
-          operation_type == myArray[j];
-      }
-    }
-  }
-
-  for(var i=0;i<lenarr;i++){
-    for(var b=0;b<lensub;b++){
-      if(myArray[i]==subtraction[b]){
-          operation_type == myArray[i];
-      }
-    }
-  }
-
-  for(var k=0;k<lenarr;k++){
-    for(var c=0;c<lenmul;c++){
-      if(myArray[k]==multiplication[c]){
-          operation_type == myArray[k];
-      }
-    }
-  }
 
   
   let result;
   const calculate = new Calculator(x, y);
+  const really = new value(operation_type,addition,subtraction,multiplication,myArray);
 
-  if (addition.indexOf(operation_type) > -1) {
+  if (addition.indexOf(really) > -1) {
     result = calculate.add;
     operation_type = "addition";
   }
-  if (subtraction.indexOf(operation_type) > -1) {
+  if (subtraction.indexOf(really) > -1) {
     result = calculate.subtract;
     operation_type = "subtraction";
   }
-  if (multiplication.indexOf(operation_type) > -1) {
+  if (multiplication.indexOf(really) > -1) {
     result = calculate.multiply;
     operation_type = "multiplication";
   }
 
   res.status(200).json({
     slackUsername: "cchimdindu",
-    operation_type: operation_type,
+    operation_type: really,
     result: Number(result),
   });
 });
+
+class value{
+  constructor(val,arr1,arr2,arr3,arr4){
+  this.val = val;
+  this.addition = arr1;
+  this.subtraction = arr2;
+  this.multiplication = arr3;
+  this.myArray = arr4;
+}
+get val(){
+  return this.calvalue;
+}
+
+calvalue(){
+  let lenadd = this.addition.length;
+  let lensub = this.subtraction.length;
+  let lenmul = this.multiplication.length;
+  let lenarr = this.myArray.length;
+
+  for(var j=0;j<lenarr;j++){
+    for(var a=0;a<lenadd;a++){
+      if(this.myArray[j]==this.addition[a]){
+          this.val == this.myArray[j];
+      }
+    }
+  }
+
+  for(var i=0;i<lenarr;i++){
+    for(var b=0;b<lensub;b++){
+      if(this.myArray[i]==this.subtraction[b]){
+          this.val == this.myArray[i];
+      }
+    }
+  }
+
+  for(var k=0;k<lenarr;k++){
+    for(var c=0;c<lenmul;c++){
+      if(this.myArray[k]==this.multiplication[c]){
+          this.val == this.myArray[k];
+      }
+    }
+  }
+  return this.value
+  }
+}
+
+
 class Calculator {
   constructor(x, y) {
     this.x = x;

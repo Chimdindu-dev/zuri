bc823485ca1a9d46770a0e2c03e2475e52e63272
Chimdindu-dev <cchimdindu@gmail.com>
2022-11-05 07:03:00 +0200

    update

--- app.js
+++ app.js
@@ -1,75 +1,20 @@
 const cors = require("cors");
 const express = require("express");
-const app = express();
+const server = express();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cors());
+server.use(express.json());
+server.use(express.urlencoded({ extended: false }));
+server.use(cors());
 
-// app.get("/", (req, res) => {
-//   res.status(200).json({
-//     slackUsername: "cchimdindu",
-//     backend: true,
-//     age: 29,
-//     bio: "I'm passionate about growth",
-//   });
-// });
-
-app.post("/", (req, res) => {
+server.post("/", (req, res) => {
   console.log(req.body);
   let { operation_type, x, y } = req.body;
 
-  const addition = [
-    "add",
-    "top up",
-    "positive",
-    "join",
-    "plus",
-    "combine",
-    "+",
-    "sum",
-    "addition",
-  ];
-  const subtraction = [
-    "minus",
-    "short of",
-    "negative",
-    "smaller",
-    "less",
-    "remove",
-    "subtract",
-    "-",
-    "deficient",
-    "subtraction",
-  ];
+  const addition = ["add","top up","positive","join","plus","combine","+","sum","addition",];
+  const subtraction = ["minus","negative","smaller","less","remove","subtract","-","deficient","subtraction",];
   const multiplication = ["times", "*", "multiply", "multiplication"];
-  const division = [
-    "divide",
-    "/",
-    "by",
-    "split",
-    "cut up",
-    "cut",
-    "cleave",
-    "carve up",
-    "slice up",
-    "chop up",
-    "split up",
-    "dissect",
-    "bisect",
-    "halve",
-    "quarter",
-    "sunder",
-    "rive",
-    "fractionate",
-    "disjoin",
-    "diverge",
-    "division",
-  ];
-
   let result;
-
-  const calculate = new Calculator(x, y);
+  const calculate = new Cal(x, y);
 
   if (addition.indexOf(operation_type) > -1) {
     result = calculate.add;
@@ -90,11 +35,11 @@ app.post("/", (req, res) => {
 
   res.status(200).json({
     operation_type: operation_type,
-    slackUsername: "ogmaro",
+    slackUsername: "cchimdindu",
     result: Number(result),
   });
 });
-class Calculator {
+class Cal {
   constructor(x, y) {
     this.x = x;
     this.y = y;
@@ -105,9 +50,6 @@ class Calculator {
   get subtract() {
     return this.subtraction();
   }
-  get divide() {
-    return this.division();
-  }
   get multiply() {
     return this.multiplication();
   }
@@ -117,9 +59,6 @@ class Calculator {
   subtraction() {
     return this.toDecimal(this.x - this.y);
   }
-  division() {
-    return this.toDecimal(this.x / this.y);
-  }
   multiplication() {
     return Number(this.toDecimal(this.x * this.y));
   }
@@ -128,6 +67,6 @@ class Calculator {
   }
 }
 
-app.listen(process.env.PORT ||1234, function(){
+server.listen(process.env.PORT ||1234, function(){
     console.log("listening on port 1234");
 });
